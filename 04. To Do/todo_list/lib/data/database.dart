da314a81b7f41e4e31d610ed228fdd851122ebed
Rishikesh <rishikeshpanditrp@gmail.com>
2023-05-24 23:58:19 +0530

    swap the buttons

--- 04. To Do/todo_list/lib/data/database.dart
+++ 04. To Do/todo_list/lib/data/database.dart
@@ -6,22 +6,6 @@ class ToDoDataBase {
   // reference our box
   final _myBox = Hive.box('mybox');
 
-  //  // reorder method
-  // void updateMyTiles(int oldIndex, int newIndex) {
-  //   setState(() {
-  //     // an adjustment is needed when moving the tile down the list
-  //     if (oldIndex < newIndex) {
-  //       newIndex--;
-  //     }
-
-  //     // get the tile we are moving
-  //     final tile = myTiles.removeAt(oldIndex);
-
-  //     // place the tile in the new position
-  //     myTiles.insert(newIndex, tile);
-  //   });
-  // }
-
   // run this method if this is the 1st time ever opening this app
   void createInitialData() {
     todoList = [
